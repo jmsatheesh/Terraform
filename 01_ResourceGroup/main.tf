285492585cc78b4cf3679f7812688f3103e72982
SatheeshKumar <jmsatheesh@gmail.com>
2024-02-20 19:23:15 +0000

    .gitignore is now working

--- 01_ResourceGroup/main.tf
+++ 01_ResourceGroup/main.tf
@@ -1,19 +1,23 @@
 provider "azurerm" {
-    features {
-      
-    }
+  features {
+
+  }
 }
 
 terraform {
   required_providers {
-    azurerm ={
-        source = "hashicorp/azurerm"
-        version = "2.40.0"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.40.0"
     }
   }
 }
 resource "azurerm_resource_group" "rg" {
-    name ="rg_Satheesh_tf"
-    location = "westeurope"
-  
+  name     = "rg_Satheesh_tf"
+  location = "westeurope"
+  tags  ={
+    Environmnent = "Prod"
+    Owner="jmsatheesh@gmail.com"
+  }
+
 }
